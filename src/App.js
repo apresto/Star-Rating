5798d02a0d9356dd5b0a763132323dd5dbb163d9
Preston Hagen <prestonhagen96@gmail.com>
2022-06-08 16:57:26 -0500

    Star Rating Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+
+const EMPTY_STAR =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1088px-Five-pointed_star.svg.png";
+const FULL_STAR =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <StarRating />;
 }
+
+const StarRating = () => {
+  const starArr = [1, 2, 3, 4, 5];
+  const [hovered, setHovered] = useState(0);
+  const [clicked, setClicked] = useState(0);
+
+  const setImg = (id) => {
+    return hovered >= id || clicked >= id ? FULL_STAR : EMPTY_STAR;
+  }
+
+  return (
+    <div
+      style={{
+        margin: '10',
+        display: 'flex',
+        justifyContent: 'center',
+        width: '50'
+      }}
+    >
+      {starArr.map((id) => (
+        <img
+          src={setImg(id)}
+          onMouseEnter={() => {
+            setHovered(id)
+            if (id < clicked) setClicked(0);
+          }}
+          onClick={() => setClicked(id)}
+          onMouseOut={() => setHovered(0)}
+          width='80'
+          height='80'
+          alt='stars'
+        />
+      ))}
+    </div>
+  )
+};
 
 export default App;
